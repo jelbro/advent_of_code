113cc78a28c6aaf761b41d0e78987fa415a62c79
Jelbro <Jelbro97@gmail.com>
2024-12-03 10:39:26 +0000

    Change file structure

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,10 +1,20 @@
+import re
+
+
 def main():
-    INPUT_FILE = 'input.txt'
+    INPUT_FILE = "input.txt"
+    pattern = r"mul\((\d+),(\d+)\)"
+    correct_patterns = 0
 
-    with open(INPUT_FILE, 'r') as file:
-        for char in file:
-            if 
+    with open(INPUT_FILE, "r") as file:
+        match = re.search(pattern, file)
+        if match:
+            correct_patterns += match.group(1) * match.group(2)
+        else:
+            pass
+
+    print(correct_patterns)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
